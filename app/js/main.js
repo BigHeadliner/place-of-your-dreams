e5653df4d4fd140dd2a279a6da45ed2bcfe552bc
BigHeadliner <bigheadliner@gmail.com>
2023-05-11 00:12:18 +0300

    header section ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(function () { 
-     
-   
+       
+      $('.header-inner__slider').slick({
+        dots: false, 
+        arrows: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // centerMode: true,
+        variableWidth: true
+      });
 
 });
