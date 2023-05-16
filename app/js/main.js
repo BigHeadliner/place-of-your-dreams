7c67b844886de0f7b6035816f3ccc62a3faf42b5
BigHeadliner <bigheadliner@gmail.com>
2023-05-16 22:06:22 +0300

    blog section ready

--- app/js/main.js
+++ app/js/main.js
@@ -48,16 +48,16 @@ $(function () {
  
  }); 
   
- $('.ready-to-sell__slider').slick({
-   dots: false,
-   arrows: false,
-   infinite: true,
-   slidesToShow: 1,
-   slidesToScroll: 1,
-   autoplay: true,
-   autoplaySpeed: 5000, 
-   pauseOnHover: true,
-}); 
+   $('.ready-to-sell__slider').slick({
+     dots: false,
+     arrows: false,
+     infinite: true,
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     autoplay: true,
+     autoplaySpeed: 5000, 
+     pauseOnHover: true,
+  }); 
  
 $("#star, #star-2, #star-3").rateYo({  
   numStars: 1,
@@ -68,15 +68,15 @@ $("#star, #star-2, #star-3").rateYo({
 
 });   
  
-$('.review-inner__slider').slick({
-    dots: true, 
-    arrows: false,
-    speed: 300, 
-    variableWidth: true,
-    slidesToShow: 1,
-    centerMode: true,  
-    centerPadding: '60px',
-});
+ $('.review-inner__slider').slick({
+     dots: true, 
+     arrows: false,
+     speed: 300, 
+     variableWidth: true,
+     slidesToShow: 1,
+     centerMode: true,  
+     centerPadding: '60px',
+ });
 
 
 
